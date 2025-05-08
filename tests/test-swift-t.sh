c22886d32f67dae4f87ea71a7f0219b5f2372014
Justin Wozniak <woz@anl.gov>
2025-05-08 13:13:32 -0500

    Better output

--- tests/test-swift-t.sh
+++ tests/test-swift-t.sh
@@ -7,16 +7,21 @@
 #         This is good for interactive use
 #         when an environment is already activated.
 
+log()
+{
+  print "test-swift-t.sh:" ${*}
+}
+
+log "START"
+
 E=""
 USE_ENV=1
 zparseopts -D -E E=E
 if (( ${#E} )) USE_ENV=0
 
-echo "test-swift-t.sh: START"
-
 if (( ${#} != 1 ))
 then
-    echo "test-swift-t.sh: Provide Python version!"
+    log "Provide Python version!"
     exit 1
 fi
 
@@ -26,11 +31,11 @@ THIS=$( dirname $0 )
 # Are we running under an automated testing environment?
 if (( ${#JENKINS_URL} > 0 ))
 then
-    echo "test-swift-t.sh: detected auto test Jenkins"
+    log "detected auto test Jenkins"
     AUTO_TEST="Jenkins"
 elif (( ${GITHUB_ACTIONS:-false} == true ))
 then
-    echo "test-swift-t.sh: detected auto test GitHub"
+    log "detected auto test GitHub"
     AUTO_TEST="GitHub"
 else
     # Other- possibly interactive user run.  Set to empty string.
@@ -41,8 +46,7 @@ if [[ $AUTO_TEST == "Jenkins" ]]
 then
     # CELS Jenkins environment
     CONDA_BIN_DIR=$WORKSPACE/../EMEWS-Conda/Miniconda-311_23.11.0-1/bin
-    PATH=$CONDA_BIN_DIR:$PATH
-    # Otherwise, we are on GitHub, and GitHub provides python, conda
+    PATH=${CONDA_BIN_DIR:a}:$PATH
 elif [[ $AUTO_TEST == "GitHub" ]]
 then
     # CONDA_EXE is set by conda
@@ -50,25 +54,28 @@ then
     # conda    is in $CONDA_HOME/condabin
     # activate is in $CONDA_HOME/bin
     CONDA_HOME=$(dirname $(dirname $CONDA_EXE))
+
     CONDA_BIN_DIR=$CONDA_HOME/bin
 
     # Placeholder with no content:
     echo > test.log
+else
+    : Assume user set up Anaconda!
 fi
 
 # Optionally activate the environment in which EMEWS was installed:
 if (( USE_ENV )) {
     ENV_NAME=emews-py${PY_VERSION}
 
-    echo "activating: $CONDA_BIN_DIR/activate '$ENV_NAME'"
+    log "activating: $CONDA_BIN_DIR/activate '$ENV_NAME'"
     if ! [[ -f $CONDA_BIN_DIR/activate ]]
     then
-        echo "File not found: '$CONDA_BIN_DIR/activate'"
+        log "File not found: '$CONDA_BIN_DIR/activate'"
         exit 1
     fi
     if ! source $CONDA_BIN_DIR/activate $ENV_NAME
     then
-        echo "could not activate: $ENV_NAME"
+        log "could not activate: $ENV_NAME"
         exit 1
     fi
 }
@@ -80,18 +87,18 @@ then
     # See code/install/README.  Sync this with install_emews.sh
     COLON=${LD_LIBRARY_PATH:+:} # Conditional colon
     export LD_LIBRARY_PATH=$CONDA_PREFIX/x86_64-conda-linux-gnu/lib$COLON${LD_LIBRARY_PATH:-}
-    echo "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+    log "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 fi
 
 PYTHON_EXE=$(which python)
 ENV_HOME=$(dirname $(dirname $PYTHON_EXE))
 
-echo "python:  " $PYTHON_EXE
-echo "version: " $(python -V)
-echo "conda:   " $(which conda)
-echo "env:     " $ENV_HOME
-echo "Rscript: " $(which Rscript)
-echo "version: " $(Rscript --version)
+log "python:  " $PYTHON_EXE
+log "version: " $(python -V)
+log "conda:   " $(=which conda)
+log "env:     " $ENV_HOME
+log "Rscript: " $(which Rscript)
+log "version: " $(Rscript --version)
 
 # EQ/R files EQR.swift and pkgIndex.tcl should be under ENV/lib:
 SWIFT_LIBS=$ENV_HOME/lib
@@ -104,11 +111,11 @@ FLAGS=( -n 4 -I $SWIFT_LIBS -r $SWIFT_LIBS )
 setopt LOCAL_OPTIONS
 () {
     # Anonymous function for 'set -x'
-    # For 'set -x' including newline:
-    set -x
     setopt LOCAL_OPTIONS
+    # For 'set -x' including newline:
     PS4="
 TEST: "
+    set -x
 
     which swift-t
     swift-t -v
@@ -120,8 +127,8 @@ TEST: "
     swift-t $FLAGS $THIS/test-apps-2.swift
 }
 
-echo "..."
-echo "test-swift-t.sh: STOP: OK"
+log "..."
+log "STOP: OK"
 
 if [[ $AUTO_TEST == "GitHub" ]]
 then
