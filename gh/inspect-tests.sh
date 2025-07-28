469b279a4b97b572ccf806bef0a0d3e83d42ccf7
Justin Wozniak <woz@anl.gov>
2025-07-28 14:07:44 -0500

    Reorder replay output

--- gh/inspect-tests.sh
+++ gh/inspect-tests.sh
@@ -9,8 +9,10 @@ msg()
   echo "inspect-tests.sh:" ${*}
 }
 
-TEST_LOG=test.log
+# Created by install_emews.sh:
 INSTALL_LOG=code/install/emews_install.log
+# Created by test-swift-t.sh:
+TEST_LOG=test.log
 
 if ! [ -f $TEST_LOG ]
 then
@@ -24,17 +26,17 @@ then
   echo
   msg "TESTS FAILED"
   echo
-  echo "TEST LOG: $TEST_LOG"
-  echo
-  cat $TEST_LOG
-  echo
-  echo "TEST LOG END: $TEST_LOG"
-  echo
   echo "INSTALL LOG BEGIN: $INSTALL_LOG"
   echo
   cat $INSTALL_LOG
   echo
   echo "INSTALL LOG END: $INSTALL_LOG"
+  echo
+  echo "TEST LOG: $TEST_LOG"
+  echo
+  cat $TEST_LOG
+  echo
+  echo "TEST LOG END: $TEST_LOG"
   echo
   exit 1
 fi
