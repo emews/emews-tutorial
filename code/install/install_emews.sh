a5ec43fe51661c3a47a9790014a3ba7629cd94ca
Justin Wozniak <woz@anl.gov>
2026-01-26 10:06:13 +0900

    Anaconda: Depend only on eq-r

--- code/install/install_emews.sh
+++ code/install/install_emews.sh
@@ -262,7 +262,7 @@ conda-list 1
 #      to Anaconda.  2025-05-08
 TEXT="Installing EMEWS Queues for R"
 start_step "$TEXT"
-conda install -y $QUIET -c conda-forge -c swift-t eq-r "swift-t-r==1.6.6" >> "$EMEWS_INSTALL_LOG" 2>&1 || on_error "$TEXT" "$EMEWS_INSTALL_LOG"
+conda install -y $QUIET -c conda-forge -c swift-t eq-r >> "$EMEWS_INSTALL_LOG" 2>&1 || on_error "$TEXT" "$EMEWS_INSTALL_LOG"
 end_step "$TEXT"
 
 conda-list 2
